5f78c4c3527e1772e0e4523018b2177165693ee7
DROMMER Fabien <Fabiendro@outlook.fr>
2020-02-13 20:00:26 +0100

    deconnexion

--- js/mod_connexion.js
+++ js/mod_connexion.js
@@ -2,6 +2,11 @@ function allerSurAuth(e) {
     document.location.href="connect";
 }
 
+function deco(e) {
+    document.location.href="logout";
+}
+
 export function init(){
     $('#connexion').click(allerSurAuth);
+    $('#deconnexion').click(deco);
 }
